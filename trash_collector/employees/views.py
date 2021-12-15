01faba34b1f33112aeca5452f777b9e98c2f5841
Joshua-Dela-Pena <richmelldelapena@gmail.com>
2021-12-15 09:51:38 -0600

    Swapped positioning of some functions

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -17,6 +17,18 @@ def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Employee = apps.get_model('employees.Employee')
     return render(request, 'employees/index.html')
+@login_required
+def create(request):
+    logged_in_user = request.user
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        new_employee = Employee(name=name_from_form, user=logged_in_user, address=address_from_form, zip_code=zip_from_form)
+        new_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        return render(request, 'employees/create.html')
 
 @login_required
 def edit_profile(request):
@@ -36,23 +48,10 @@ def edit_profile(request):
             'logged_in_employee': logged_in_employee
         }
         return render(request, 'employees/edit_profile.html', context)
-
-@login_required
-def create(request):
-    logged_in_user = request.user
-    if request.method == "POST":
-        name_from_form = request.POST.get('name')
-        address_from_form = request.POST.get('address')
-        zip_from_form = request.POST.get('zip_code')
-        new_employee = Employee(name=name_from_form, user=logged_in_user, address=address_from_form, zip_code=zip_from_form)
-        new_employee.save()
-        return HttpResponseRedirect(reverse('employee:index'))
-    else:
-        return render(request, 'employee/create.html')
     
 @login_required
 def my_pickups(request):
-    logged_in_employee = request.employee
+    logged_in_user = request.user
     logged_in_employee = Employee.objects.get(employee=logged_in_user)
     if request.method == "POST":
         date_from_form = request.POST.get('date')
